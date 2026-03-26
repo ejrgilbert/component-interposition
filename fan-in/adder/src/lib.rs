5a11d2ea941fc978ba10cd7b3ecef8d21e4b4c9a
Elizabeth Gilbert <elizabeth@cosmonic.com>
2026-03-26 16:50:47 -0400

    Add crates that can be composed into the fan-in topology

---
+++ fan-in/adder/src/lib.rs
@@ -0,0 +1,28 @@
+mod bindings {
+    wit_bindgen::generate!({
+        world: "service",
+        async: true,
+        generate_all
+    });
+}
+use crate::bindings::exports::my::service::adder::Guest;
+
+pub struct Service;
+
+impl Guest for Service {
+    async fn add(
+        a: i32, b: i32
+    ) -> i32 {
+
+        println!("    [adder] entered!");
+
+        let res = a + b;
+
+        println!("    [adder] exit!");
+
+        res
+    }
+}
+
+// Export the component
+bindings::export!( Service with_types_in bindings );
---
+++ fan-in/printer1/src/lib.rs
@@ -0,0 +1,23 @@
+mod bindings {
+    wit_bindgen::generate!({
+        world: "service",
+        async: true,
+        generate_all
+    });
+}
+use crate::bindings::exports::my::service::printer1::Guest;
+
+pub struct Service;
+
+impl Guest for Service {
+    async fn print1(
+        msg: String
+    ) {
+        println!("    [print1] entered!");
+        println!("        {msg}");
+        println!("    [print1] exit!");
+    }
+}
+
+// Export the component
+bindings::export!( Service with_types_in bindings );
